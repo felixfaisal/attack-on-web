65f0305a40000dfadfd38f6f9e8369bc45156325
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-02-12 19:54:16 +0530

    Added a trigger string

--- file-reader/index.js
+++ file-reader/index.js
@@ -1,16 +1,28 @@
 const fs = require('fs');
-let directory_name = ".";
-let filenames = fs.readdirSync(directory_name);
-console.log("\nFilenames in the directory:");
-filenames.forEach((file) => {
-    let fileName = directory_name + "/" + file;
-    fs.readFile(fileName, 'utf8', (err, data) => {
-        if (err) {
-            console.log(err);
-        }
-        console.log("Reading File: " + fileName);
-        console.log(data);
-        console.log(typeof x);
+const { exit } = require('process');
+
+const fileReader = () => {
+    let directory_name = ".";
+    let filenames = fs.readdirSync(directory_name);
+    console.log("\nFiles in the directory:");
+    filenames.forEach((file) => {
+        let fileName = directory_name + "/" + file;
+        fs.readFile(fileName, 'utf8', (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log("Reading File: " + fileName);
+            // console.log(data);
+            let result = data.search('index.js')
+            if (result) {
+                console.log("Found index.js in file " + fileName)
+                exit(0)
+            }
+            // console.log(typeof data);
+        });
+        console.log("File:", file);
     });
-    console.log("File:", file);
-});
+
+}
+fileReader();
+
