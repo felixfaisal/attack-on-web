cadb8c3e2c6089c533d7e8eb67e33c474914c29a
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-02-15 22:17:47 +0530

    Dynamically pulling data from json

--- src/extensionChecker.js
+++ src/extensionChecker.js
@@ -1,46 +1,18 @@
+const { defaultCipherList } = require('constants');
 const fs = require('fs')
 // import extension from '/extensions.json'
 
 const extensionChecker = (fileName) => {
-    let regexp = /\.(js|ts|json)$/gi
-    // let str = "extension.gif"
-    let result = fileName.search(regexp);
-    if (result != -1) {
-        // console.log("Matched extension");
-        return 1;
+    let rawdata = fs.readFileSync('extensions.json');
+    let exten = JSON.parse(rawdata);
+    let result = 0
+    for (x in exten.fileExtensions) {
+        if (fileName.endsWith(exten.fileExtensions[x])) {
+            result = 1;
+            break;
+        }
     }
-    else {
-        // console.log("Didn't match extension");
-        return 0;
-    }
+    return result
+
 }
-// const fs = require('fs');
-
-let rawdata = fs.readFileSync('extensions.json');
-let student = JSON.parse(rawdata);
-console.log(student.fileExtensions);
-let regString = '/\.(js)$/gi';
-console.log(regString)
-let newString = regString.slice(0, 5)
-    + "|ts"
-    + regString.slice(5);
-
-let regexp = new RegExp(newString, "gi");
-let regx = /\.(js|ts|json)$/gi;
-console.log(regx)
-let str = "extensions.js"
-console.log(str.search(/\.(js|ts|json)$/gi));
-console.log(regexp);
-
-console.log(newString);
-console.log(regString[5])
-for (x in student.fileExtensions) {
-    console.log(student.fileExtensions[x]);
-}
-
-// console.log(extension.fileExtensions)
-// export default extensionChecker;
-
-/* console.log(extensionChecker('extension.gif'));
-console.log(extensionChecker('index.js'));
-console.log(extensionChecker('new.json')); */
+export default extensionChecker;
