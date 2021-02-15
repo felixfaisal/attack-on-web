0d7ca3fb02f6e2476ff162dd774a3017d8e45d57
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-02-15 20:57:22 +0530

    Created JSON for a single data point

--- src/extensionChecker.js
+++ src/extensionChecker.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+// import extension from '/extensions.json'
 
 const extensionChecker = (fileName) => {
     let regexp = /\.(js|ts|json)$/gi
@@ -13,8 +14,32 @@ const extensionChecker = (fileName) => {
         return 0;
     }
 }
+// const fs = require('fs');
 
-export default extensionChecker;
+let rawdata = fs.readFileSync('extensions.json');
+let student = JSON.parse(rawdata);
+console.log(student.fileExtensions);
+let regString = '/\.(js)$/gi';
+console.log(regString)
+let newString = regString.slice(0, 5)
+    + "|ts"
+    + regString.slice(5);
+
+let regexp = new RegExp(newString, "gi");
+let regx = /\.(js|ts|json)$/gi;
+console.log(regx)
+let str = "extensions.js"
+console.log(str.search(/\.(js|ts|json)$/gi));
+console.log(regexp);
+
+console.log(newString);
+console.log(regString[5])
+for (x in student.fileExtensions) {
+    console.log(student.fileExtensions[x]);
+}
+
+// console.log(extension.fileExtensions)
+// export default extensionChecker;
 
 /* console.log(extensionChecker('extension.gif'));
 console.log(extensionChecker('index.js'));
