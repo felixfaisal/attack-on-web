3d2877afcac840bacbc634557389ca2db7bfa3b2
Anjali <anjali001.as@gmail.com>
2021-02-16 19:43:46 +0530

    modified check

--- src/check.js
+++ src/check.js
@@ -2,8 +2,19 @@ const fs = require('fs');
 path = require('path');
 
 const arr = fs.readFileSync('../\.gitignore').toString().split("\n")
+let val;
+// console.log('arr', arr)
+arr.map(item => {
+    if(item[0]=='/')
+    {
+       val = item.slice(1);
+       let index = arr.indexOf(item);
+       arr[index] = val
+    }
+})
+// // console.log('val', val)
+// console.log("sliced array",arr);
 
-console.log('arr', arr)
 
 
 function crawl(dir){
