d923d5a2a7b6b86948d5daaca7e7d0eb2782f023
Anjali <anjali001.as@gmail.com>
2021-02-23 15:00:41 +0530

    list of commands

--- index.js
+++ index.js
@@ -130,7 +130,8 @@ if (argv.test == true) {
 Hello, welcome to attack-on-web! AoW is a tool to test security vulnerabilities in your code.
 
 ${chalk.yellow('Tip:')} Type ${chalk.white('aow -h')} or ${chalk.white('aow --help')} to view list of commands
-     Type ${chalk.white('aow -t')} or ${chalk.white('aow --test')} for testing.`))
+     Type ${chalk.white('aow -t')} or ${chalk.white('aow --test')} for testing.
+     Type ${chalk.white('aow -l')} or ${chalk.white('aow --list')} for list of providers.`))
 
 }
 
